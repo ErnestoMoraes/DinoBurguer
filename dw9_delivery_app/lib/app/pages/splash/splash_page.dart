86bc2b6b02157ab28ef0e24030e8e7e13864e119
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-30 18:39:40 -0300

    Theme 100% configurado

---
+++ dw9_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(onPressed: () {}, child: const Text('Teste')),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'Nome',
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
