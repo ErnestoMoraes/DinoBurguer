c0d4c0881fa445d4269332153c1ec8af73d723fc
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-01 19:56:28 -0300

    Ver sacola esta funcional

---
+++ dw9_delivery_app/lib/app/pages/home/widgets/shopping_bag_widget.dart
@@ -0,0 +1,65 @@
+import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
+import 'package:flutter/material.dart';
+
+class ShoppingBagWidget extends StatelessWidget {
+  final List<OrderProductDto> bag;
+
+  const ShoppingBagWidget({
+    Key? key,
+    required this.bag,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var totalBag = bag.fold<double>(
+      0.0,
+      (total, element) => total += element.totalPrice,
+    ).currencyPTBR;
+
+    return Container(
+      width: context.screenWidth,
+      height: 90,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade400,
+            blurRadius: 5,
+          ),
+        ],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Stack(
+          children: [
+            const Align(
+                alignment: Alignment.centerLeft,
+                child: Icon(Icons.shopping_cart_rounded)),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Ver sacola',
+                style: context.textStyles.textExtraBold,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                totalBag,
+                style: context.textStyles.textExtraBold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
