58ded7d28464b5792fbdbaf2ba0bb046b132b55b
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-01 15:34:02 -0300

    Button increment e decrement funcionando

--- dw9_delivery_app/lib/app/pages/product_detail/product_detail_page.dart
+++ dw9_delivery_app/lib/app/pages/product_detail/product_detail_page.dart
@@ -1,13 +1,28 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
+import 'package:dw9_delivery_app/app/core/ui/base_state/base_state.dart';
 import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_increment_decrement_button.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductDetailPage extends StatelessWidget {
-  const ProductDetailPage({Key? key}) : super(key: key);
+class ProductDetailPage extends StatefulWidget {
+  final ProductModel product;
+  const ProductDetailPage({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
 
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState
+    extends BaseState<ProductDetailPage, ProductDetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +33,11 @@ class ProductDetailPage extends StatelessWidget {
           Container(
             width: context.screenWidth,
             height: context.percentHeight(0.4),
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
                 image: NetworkImage(
-                    'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'),
+                  widget.product.image,
+                ),
                 fit: BoxFit.cover,
               ),
             ),
@@ -32,19 +48,19 @@ class ProductDetailPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Text(
-              'X-Burguer',
+              widget.product.name,
               style: context.textStyles.textExtraBold.copyWith(fontSize: 22),
             ),
           ),
           const SizedBox(
             height: 10,
           ),
-          const Expanded(
+          Expanded(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               child: SingleChildScrollView(
                 child: Text(
-                  'Lanche acompanha pão, hambuerguer, mussarela e maionese',
+                  widget.product.description,
                 ),
               ),
             ),
@@ -56,41 +72,53 @@ class ProductDetailPage extends StatelessWidget {
                 height: 68,
                 width: context.percentWidth(0.5),
                 padding: const EdgeInsets.all(8),
-                child:  DeliveryIncrementDecrementButton(
-                  decrementTap: (){debugPrint('decrement');},
-                  incrementTap: () {debugPrint('increment');},
-                  amount: 5,
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return DeliveryIncrementDecrementButton(
+                      decrementTap: () {
+                        controller.decrement();
+                      },
+                      incrementTap: () {
+                        controller.increment();
+                      },
+                      amount: amount,
+                    );
+                  },
                 ),
               ),
               Container(
                 width: context.percentWidth(0.5),
                 height: 68,
                 padding: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Adicionar',
-                        style: context.textStyles.textExtraBold
-                            .copyWith(fontSize: 12),
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Adicionar',
+                            style: context.textStyles.textExtraBold
+                                .copyWith(fontSize: 12),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: AutoSizeText(
+                              (widget.product.price * amount).currencyPTBR,
+                              maxFontSize: 13,
+                              minFontSize: 5,
+                              maxLines: 1,
+                              textAlign: TextAlign.center,
+                              style: context.textStyles.textExtraBold,
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: AutoSizeText(
-                          r'R$ 60,99',
-                          maxFontSize: 13,
-                          minFontSize: 5,
-                          maxLines: 1,
-                          textAlign: TextAlign.center,
-                          style: context.textStyles.textExtraBold,
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               )
             ],
