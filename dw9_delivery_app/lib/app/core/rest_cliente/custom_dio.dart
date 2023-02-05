8efea652e8de265e184d2b1e6a378e8c398ced07
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-05 13:26:14 -0300

    Implementando authinterceptor para adiconar token na requisição

--- dw9_delivery_app/lib/app/core/rest_cliente/custom_dio.dart
+++ dw9_delivery_app/lib/app/core/rest_cliente/custom_dio.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:dw9_delivery_app/app/core/config/env/env.dart';
+import 'package:dw9_delivery_app/app/core/rest_cliente/interceptors/auth_interceptor.dart';
 
 class CustomDio extends DioForNative {
+  late AuthInterceptor _authInterceptor;  
   CustomDio()
       : super(BaseOptions(
           baseUrl: Env.instance['backend_base_url'] ?? '',
@@ -16,13 +18,16 @@ class CustomDio extends DioForNative {
         requestHeader: true,
       ),
     );
+    _authInterceptor = AuthInterceptor();
   }
 
   CustomDio auth() {
+    interceptors.add(_authInterceptor);
     return this;
   }
 
   CustomDio unauth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
