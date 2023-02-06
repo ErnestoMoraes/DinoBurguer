e891ec1ca79e7473b601b3eec8a0dad3c90b9ccc
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-06 18:00:20 -0300

    Fazendo a tela de pedido realizado com sucesso

--- dw9_delivery_app/lib/app/pages/order/order_completed_page.dart
+++ dw9_delivery_app/lib/app/pages/order/order_completed_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
 class OrderCompletedPage extends StatelessWidget {
+  const OrderCompletedPage({super.key});
 
-  const OrderCompletedPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Text('Sucesso!!'),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(),
+    );
   }
 }
